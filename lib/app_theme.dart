83b70ca5494ad63da2e62bdbd43673cc12c21235
ahmad <asalameh02gmail.comm>
2024-12-17 00:35:03 +0200

    finish splash

---
+++ lib/app_theme.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+ class AppTheme{
+  static const Color primary=Color(0xff39A552) ;
+  static const Color white=Color(0xffFFFFFF);
+  static const Color black=Color(0xff303030);
+  static const Color navy=Color(0XFF4F5A69);
+
+
+  static ThemeData lightTheme=ThemeData(
+
+  );
+}
